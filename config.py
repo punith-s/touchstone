f7d63255dda7d18768964f080b02f3981bb7422f
punith-cloudbyte <punith.s@cloudbyte.com>
2015-08-21 16:03:22 +0530

    adding config file

---
+++ config.py
@@ -0,0 +1,21 @@
+__author__ = 'punith'
+
+# Elasticenter api url and api key
+api_url = 'https://20.10.69.69/client/api'
+apiKey = 'XxjsEhBztSI6cxxbNiGTFyPRmslJY1l3n87sKixiXXKnJjMwJz6zEYbXtJRpOh37gUFH237thTkjpFie3lCFDg'
+
+# params for list tsm
+listTsmRequest = {
+    'type': 'all'
+}
+
+#Add volume Group test
+
+addVolumeGroupTest_1 = {
+    'name': 'test1'
+}
+
+addVolumeGroupTest_2 = {
+    'name': 'test2',
+    'description': 'adding the volume group'
+}
