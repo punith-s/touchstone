88fa094096312fd014ec935e550e4c094ca19d51
punith-cloudbyte <punith.s@cloudbyte.com>
2015-08-21 18:06:39 +0530

    adding yaml config file support

--- Caller.py
+++ Caller.py
@@ -4,15 +4,19 @@ from config import *
 
 from SignedAPICall import CloudByte
 import config
+import yaml
 
 api = CloudByte(api_url, apiKey)
 
+stream = file('config.yaml', 'r')
+
+yamlconfig = yaml.load(stream)
+
 # list TSM
-result = api.listTsm(listTsmRequest)
+result = api.listTsm(yamlconfig['listTsmRequest'])
 #print result
 count = result['listTsmResponse']['count']
 print "TSM ID: ", result['listTsmResponse']['listTsm'][count - 1]['id']
 
 
 # volume group tests
-
